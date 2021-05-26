b146bfa41f09b888de9d59fa21e464b4c5bcc747
parkhr <vlhong@gmail.com>
2021-05-26 23:20:43 +0900

    stream 에 대해 학습

--- src/chapter4/Test.java
+++ src/chapter4/Test.java
@@ -1,7 +1,30 @@
 package chapter4;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Test {
     public static void main(String[] args) {
+        List<Dish> menu = Arrays.asList(
+                new Dish("pork", false, 800, Dish.Type.MEAT),
+                new Dish("beef", false, 700, Dish.Type.MEAT),
+                new Dish("chicken", false, 400, Dish.Type.MEAT),
+                new Dish("french fries", true, 530, Dish.Type.OTHER),
+                new Dish("rice", true, 350, Dish.Type.OTHER),
+                new Dish("season fruit", true, 120, Dish.Type.OTHER),
+                new Dish("pizza", true, 550, Dish.Type.OTHER),
+                new Dish("prawns", false, 300, Dish.Type.FISH),
+                new Dish("salmon", false, 400, Dish.Type.FISH)
+        );
 
+        List<String> threeHighCaloricDishNames =
+                menu.stream()
+                        .filter(dish -> dish.getCalories() > 300) // filter calories
+                        .map(Dish::getName) // get name
+                        .limit(5)
+                        .collect(Collectors.toList()); // result save to new list
+
+        System.out.println(threeHighCaloricDishNames);
     }
 }
